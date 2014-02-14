3e3a7c28b569212508c8b22fd7e7c2b103459f90
Olivier Cuypers <oliviercuyp@gmail.com>
2014-02-14 16:03:42 +0100

    Add Facebook POST

--- backend/routes/facebook.js
+++ backend/routes/facebook.js
@@ -2,8 +2,8 @@
 
 var Q = require('q');
 var facebookSvc = require('../services/facebook');
-//var usersSvc = require('../services/users');
-//var friendsSvc = require('../services/friends');
+var usersSvc = require('../services/users');
+var friendsSvc = require('../services/friends');
 
 function getAuthUrl(req, res) {
   res.json(200, facebookSvc.getAuthUrl());
@@ -16,23 +16,39 @@ function createUserFromFbCode(req, res) {
       return;
     }
 
-    var promises = [
-      Q.ninvoke(facebookSvc, 'getUserInfo', token)
-        .then(function (err, userInfo) {
-          return true;
-        }),
-      Q.ninvoke(facebookSvc, 'getUserFriends', token)
-        .then(function (err, userInfo) {
-          return true;
-        })
-    ];
+    Q.ninvoke(facebookSvc, 'getUserInfo', token)
+      .then(function (userFbInfo) {
+        Q.ninvoke(usersSvc, 'getByFbId', userFbInfo.id)
+          .then(function (user) {
+            if (user !== null) {
+              return user;
+            }
+            var userData = {
+              token: token,
+              facebook_id: userFbInfo.id,
+              email: userFbInfo.email
+            };
+            return Q.ninvoke(usersSvc, 'createInfo', userData)
+              .then(function (users) {
+                user = users[0];
+                return Q.ninvoke(facebookSvc, 'getUserFriends', token);
+              })
+              .then(function (friendsInfo) {
+                var friendIds = [];
 
-    Q.all(promises)
-      .then(function (results) {
-        res.json(200, {
-          id: 1,
-          token: token
-        });
+                friendsInfo.forEach(function (friend) {
+                  friendIds.push(friendIds.id);
+                });
+
+                return Q.npost(friendsSvc, 'createInfo', [friendIds, user.id]);
+              })
+              .then(function (friendIds) {
+                res.json(200, user);
+              })
+              .fail(function (err) {
+                res.send(500);
+              });
+          });
       })
       .fail(function (err) {
         res.send(500);
