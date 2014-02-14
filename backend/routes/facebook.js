a038f5c41bf85d79553b0fd331afc3c8c28dad23
Olivier Cuypers <oliviercuyp@gmail.com>
2014-02-14 16:58:07 +0100

    Fake facebook POST

--- backend/routes/facebook.js
+++ backend/routes/facebook.js
@@ -15,46 +15,48 @@ function createUserFromFbCode(req, res) {
       res.send(500, err);
       return;
     }
-    Q.ninvoke(facebookSvc, 'getUserInfo', token)
-      .then(function (userFbInfo) {
-        console.log('USER INFO: %j', userFbInfo);
-        Q.ninvoke(usersSvc, 'getInfoByFbId', userFbInfo.id)
-          .then(function (user) {
-            console.log('USER: %j', user);
-            if (user !== null) {
-              return user;
-            }
-            var userData = {
-              token: token,
-              facebook_id: userFbInfo.id,
-              email: userFbInfo.email
-            };
-            return Q.ninvoke(usersSvc, 'createInfo', userData)
-              .then(function (users) {
-                console.log('USERS: %j', users);
-                user = users[0];
-                return Q.ninvoke(facebookSvc, 'getUserFriends', token);
-              })
-              .then(function (friendsInfo) {
-                var friendIds = [];
 
-                friendsInfo.forEach(function (friend) {
-                  friendIds.push(friendIds.id);
-                });
+    res.json(200, 1);
+    // Q.ninvoke(facebookSvc, 'getUserInfo', token)
+    //   .then(function (userFbInfo) {
+    //     console.log('USER INFO: %j', userFbInfo);
+    //     Q.ninvoke(usersSvc, 'getInfoByFbId', userFbInfo.id)
+    //       .then(function (user) {
+    //         console.log('USER: %j', user);
+    //         if (user !== null) {
+    //           return user;
+    //         }
+    //         var userData = {
+    //           token: token,
+    //           facebook_id: userFbInfo.id,
+    //           email: userFbInfo.email
+    //         };
+    //         return Q.ninvoke(usersSvc, 'createInfo', userData)
+    //           .then(function (users) {
+    //             console.log('USERS: %j', users);
+    //             user = users[0];
+    //             return Q.ninvoke(facebookSvc, 'getUserFriends', token);
+    //           })
+    //           .then(function (friendsInfo) {
+    //             var friendIds = [];
 
-                return Q.npost(friendsSvc, 'createInfo', [friendIds, user.id]);
-              })
-              .then(function (friendIds) {
-                res.json(200, user);
-              })
-              .fail(function (err) {
-                res.send(500);
-              });
-          });
-      })
-      .fail(function (err) {
-        res.send(500);
-      });
+    //             friendsInfo.forEach(function (friend) {
+    //               friendIds.push(friendIds.id);
+    //             });
+
+    //             return Q.npost(friendsSvc, 'createInfo', [friendIds, user.id]);
+    //           })
+    //           .then(function (friendIds) {
+    //             res.json(200, user);
+    //           })
+    //           .fail(function (err) {
+    //             res.send(500);
+    //           });
+    //       });
+    //   })
+      // .fail(function (err) {
+      //   res.send(500);
+      // });
   });
 }
 
